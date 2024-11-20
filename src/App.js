e30307707f4d14efcbcabae9ff6529e5a327d328
Sergio Regar <117889913+SRGio7@users.noreply.github.com>
2024-11-20 19:16:03 +0800

    Firebase Image

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import BodyCV from "./components/body";
-import React, { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
-import database from "./firebase";
 
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const dbRef = ref(database, "data-path");
-    const unsubscribe = onValue(dbRef, (snapshot) => {
-      const value = snapshot.val();
-      setData(value);
-    });
+  // useEffect(() => {
+  //   const dbRef = ref(database, "data-path");
+  //   const unsubscribe = onValue(dbRef, (snapshot) => {
+  //     const value = snapshot.val();
+  //     setData(value);
+  //   });
 
-    // Cleanup listener saat komponen unmount
-    return () => unsubscribe();
-  }, []);
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
     <div>
-      {/* {data ? (
-        <p>Nama: { data.nama}</p>
-      ) : (
-        <p>Loading data...</p>
-      )} */}
       <BodyCV />
     </div>
   );
--- src/components/body.js
+++ src/components/body.js
@@ -1,10 +1,12 @@
 // import {firebase} from "../firebase"
 import InfoDiri from "./left_sect";
 import BgKanan from "./right_sect";
+import Header from "./Header";
 
 const BodyCV = () => {
   return (
     <div className="container">
+      <Header/>
       <div className="header">
         <div className="year">2024</div>
         <div className="cover-title">CV</div>
